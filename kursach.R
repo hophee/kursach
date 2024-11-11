5fe846f22fd7f64cb607d4f72bb70230adca8b00
hophee <zaycev.i21@gmail.com>
2024-11-12 00:03:25 +0300

    Adde rollmean and raw git load

--- kursach.R
+++ kursach.R
@@ -1,23 +1,27 @@
 library(tidyr)
-library(janitor)
-library(readxl)
 library(dplyr)
-library(lubridate)
-library(ggplot2)
+library(janitor) #обработка загаловков таблицы в едином формате
+library(openxlsx) #загрузка датасета с гита
+library(lubridate) #работа с датами
+library(ggplot2) #визуализация
+library(zoo) #реализация фильтра скользящего окна
+#лучше использовать library(readxl), если загружается локально, тогда read.xlsx заменяется на read_xlsx
 
 #прочтение оригинальных данных
-spiro_data <-  read_xlsx('/home/iz-user/Documents/univer/kursach/kursovaya.xlsx') %>% 
+spiro_data <-  read.xlsx('https://raw.githubusercontent.com/hophee/kursach/main/kursovaya.xlsx', sheet=1) %>% 
   janitor::clean_names()
 
-#выделение моей даты
+#выделение нужной даты
 spyro_p11_3 <- spiro_data %>% filter(pacient == 'П11-3') %>%  select(c(1, 3:19))
 spyro_p11_3 <- spyro_p11_3 %>%   mutate(vrema_obsled = as.numeric(hms(vrema_obsled) - hms(vrema_obsled[1])))
-
-#отображение
+spyro_p11_3 <- spyro_p11_3 %>% mutate(across(rn_r50_om:lf_phi5_grad, ~ rollmean(., k = 3, fill = NA))) 
+#тут стоит подумать, как заполнить первую и последнюю строку исходными значениями
+  
+#отображение одного столбца
 ggplot(data = spyro_p11_3, aes(vrema_obsled, lf_r50_om)) +
   geom_line()
 
-#переводит фрей в лонг формат, для отображения на одной картинке (бессмысленно, они слишком разных размерностей)
+#переводит фрейм в лонг формат, для отображения на одной картинке (бессмысленно, они слишком разных размерностей)
 df_long <- spyro_p11_3 %>% 
   pivot_longer(cols = -c(pacient, vrema_obsled), names_to = "variable", values_to = "value")
 
