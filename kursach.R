af227800e1ed4c5ab34f2df445b50d678b9bf8ce
hophee <zaycev.i21@gmail.com>
2024-11-11 21:07:14 +0300

    Desription

--- kursach.R
+++ kursach.R
@@ -4,8 +4,10 @@ library(readxl)
 library(dplyr)
 library(clock)
 
-
+#прочтение оригинальных данных
 spiro_data <-  read_xlsx('/home/iz-user/Documents/univer/kursach/kursovaya.xlsx') %>% 
   janitor::clean_names()
 
+#выделение моей даты
 spyro_p11_3 <- spiro_data %>% filter(pacient == 'П11-3') %>%  select(c(1, 3:19))
+
