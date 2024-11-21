9901551bf8d1dad0f75f8579170200e99427c4f6
hophee <zaycev.i21@gmail.com>
2024-11-21 13:16:42 +0300

    Added spyrogram function, removed fft filter in other file, cleaned code

--- kursach.R
+++ kursach.R
@@ -6,15 +6,49 @@ library(lubridate) #работа с датами
 library(ggplot2) #визуализация
 library(zoo) #реализация фильтра скользящего окна
 #лучше использовать library(readxl), если загружается локально, тогда read.xlsx заменяется на read_xlsx
+#функция для фильтра сигнала, по умолчанию выставлена чатоста среза 10, частота дискретизации 50, чтобы упростить вызов под задачу
+fourie_filter <- function(signa) {
+  vector_name <- deparse(substitute(signa))
+  if (grepl("50", vector_name)) {
+    samp_rate = 50
+    cutoff_freq = 10
+  } else {
+    samp_rate = 5
+    cutoff_freq = 1
+  }
+  nyquist <- samp_rate / 2
+  cutoff <- floor(cutoff_freq / nyquist * length(signa) / 2)
+  filtered_fft <- fft(signa)
+  filtered_fft[(cutoff+1):(length(signa)-cutoff)] <- 0
+  filtered_data <- Re(fft(filtered_fft, inverse = TRUE)) / length(signa)
+  return (filtered_data)
+}
 
 #прочтение оригинальных данных
 spiro_data <-  read.xlsx('https://raw.githubusercontent.com/hophee/kursach/main/kursovaya.xlsx', sheet=1) %>% 
   janitor::clean_names()
 
-#выделение нужной даты
+#для одного случая выделение нужной даты
 spyro_p11_3 <- spiro_data %>% filter(pacient == 'П11-3') %>%  select(c(1, 3:19))
 spyro_p11_3 <- spyro_p11_3 %>% mutate(vrema_obsled = as.numeric(hms(vrema_obsled) - hms(vrema_obsled[1])))
-spyro_p11_3 <- spyro_p11_3 %>% mutate(across(rn_r50_om:lf_phi5_grad, ~ rollmean(., k = 3, fill = c((.[1]+.[2])/2, (.[length(.)]+.[length(.)-1])/2)))) 
+spyro_p11_3 <- spyro_p11_3 %>% mutate(across(rn_r50_om:lf_phi5_grad, ~ fourie_filter(.))) 
+spyro_p11_3 <- spyro_p11_3 %>% mutate(across(rn_r50_om:lf_phi5_grad, ~ rollmean(., k = 5, fill = na.fill(., "extend")))) 
+
+
+#функция получения спирограмы
+get_pacient_spyro <- function(df, pacient_code, roll_mean_num) {
+  spyro <- df %>% filter(pacient == pacient_code) %>% select(c(1, 3:19))
+  spyro <- spyro %>% mutate(vrema_obsled = as.numeric(hms(vrema_obsled) - hms(vrema_obsled[1])))
+  spyro <- spyro %>% mutate(across(rn_r50_om:lf_phi5_grad, ~ fourie_filter(.))) 
+  spyro <- spyro %>% mutate(across(rn_r50_om:lf_phi5_grad, ~ rollmean(., k = roll_mean_num, fill = na.fill(., "extend"))))
+  for (i in names(spyro)[-(1:2)]){
+    p <- ggplot(data = spyro, aes_string(x = "vrema_obsled", y = i)) +
+      geom_line()
+    print(p)
+  }
+}
+#вызов для одного пацента
+get_pacient_spyro(spiro_data, 'П13', 5)
 
 #отображение одного столбца
 ggplot(data = spyro_p11_3, aes(vrema_obsled, lf_r50_om)) +
@@ -31,20 +65,15 @@ ggplot(df_long, aes(x = vrema_obsled, y = value, color = variable)) +
        y = "Значение") +
   theme_minimal()
 
+#запись всех графиков в один лист
+plots <- lapply(3:18, function(i){
+  ggplot(spyro_p11_3, aes_string(x = 'vrema_obsled', y = names(spyro_p11_3)[i])) +
+    geom_line()
+})
+
 #делает все графики по отдельности
 for (i in names(spyro_p11_3)[-(1:2)]){
   p <- ggplot(data = spyro_p11_3, aes_string(x = "vrema_obsled", y = i)) +
     geom_line()
   print(p)
 }
-
-#хуйня какая-то
-vremya <- spyro_p11_3$vrema_obsled
-fourie <- fftfilt(vremya, spyro_p11_3$rn_r50_om)
-plot(vremya, fourie)
-
-
-test_df <- data_frame(spyro_p11_3$vrema_obsled, spyro_p11_3$rn_r50_om)
-names(test_df) <- c('t', 'rn_r50')
-plot(test_df$t, test_df$rn_r50)
-
