c6ddc10c689c3b91d35915f18dafce06a5fd7be5
hophee <zaycev.i21@gmail.com>
2024-11-30 20:07:54 +0300

    Plots of compare additional script

--- make_compare_plots.R
+++ make_compare_plots.R
@@ -26,25 +26,27 @@ only_data_get_pacient_spyro <- function(df, pacient_code, roll_mean_num, long=F,
   res <- list(spyro, before_filtering)
   return (res)
 }
+
+#поменять здесь индекс у списка пациентов, чтобы получить другой 
 df_after <- only_data_get_pacient_spyro(spiro_data, my_patients[1], 5)[[1]]
 df_before <- only_data_get_pacient_spyro(spiro_data, my_patients[1], 5)[[2]]
+
 df <- data.frame(df_before$vrema_obsled, df_before$r50_om,df_after$r50_om)
 names(df) <- c('vrema_obsled', 'before_r50_om', 'after_r50_om')
 p <- ggplot(data=df, aes(x=vrema_obsled)) +
-  geom_line(aes(y=before_r50_om, color = 'Before')) +
-  geom_line(aes(y=after_r50_om, color = 'After')) +
+  geom_line(aes(y=before_r50_om, color = 'Before'), lwd=0.8) +
+  geom_line(aes(y=after_r50_om, color = 'After'), lwd=0.9) +
   scale_color_manual(values = c("Before" = '#ef233c', "After" = '#3a86ff'),
                      name = "Измерения",
                      labels = c("До фильтрации", "После фильтрации")) +
   labs(x='Время',
        y='Объём лёгких',
-       title='Сравнение данных до и после', 
-       subtitle = 'Спирограмма, пациент П11-3')
+       title='Сравнение данных до и после фильтрации', 
+       subtitle = 'Спирограмма, пациент П11-3') #здесь поменять лэйбл
 p
-ggsave('p11_3_pnevmo_compare.png', plot = p, width = 10, height = 6, dpi=600)
+ggsave('p11_3_spiro_compare.png', plot = p, width = 10, height = 6, dpi=600)
+#здесь тоже
 
-df_after <- only_data_get_pacient_spyro(spiro_data, my_patients[1], 5)[[1]]
-df_before <- only_data_get_pacient_spyro(spiro_data, my_patients[1], 5)[[2]]
 df <- data.frame(df_before$vrema_obsled, df_before$r50_om, df_before$div_r50_om, df_after$r50_om, df_after$div_r50_om)
 names(df) <- c('vrema_obsled', 'before_r50_om', 'before_div', 'after_r50_om', 'after_div')
 p <- ggplot(data=df) +
@@ -55,7 +57,8 @@ p <- ggplot(data=df) +
                      labels = c("До фильтрации", "После фильтрации")) +
   labs(x='Объём лёгких',
        y='Объёмная скорость',
-       title='Сравнение данных до и после', 
-       subtitle = 'Пневмотахограмма, пациент П11-3')
+       title='Сравнение данных до и после фильтрации', 
+       subtitle = 'Пневмотахограмма, пациент П11-3') #и здесь
 p
 ggsave('p11_3_pnevmo_compare.png', plot = p, width = 10, height = 6, dpi=600)
+#тут тоже
